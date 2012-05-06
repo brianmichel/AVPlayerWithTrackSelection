fc6e720b4450d1748a8a25ab202783cceeef4efd
Brian Michel <brianmichel@Brian-Michels-MacBook-Pro.local>
2012-05-06 16:23:24 -0400

    Initial Commit

---
+++ AVPlayerWithTrackSelection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AVPlayerWithTrackSelection
+//
+//  Created by Brian Michel on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
